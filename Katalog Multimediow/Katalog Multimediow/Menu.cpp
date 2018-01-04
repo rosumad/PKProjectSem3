a73f1d817922f757aad40139d523af2e1dbd1508
Robert <robert.suszek96@gmail.com>
2018-01-05 00:09:15 +0100

    Menu class not fully implemented.

--- Katalog Multimediow/Katalog Multimediow/Menu.cpp
+++ Katalog Multimediow/Katalog Multimediow/Menu.cpp
@@ -5,7 +5,8 @@ Menu::Menu()
 	this->windowSize = 60;
 	this->name = "";
 	this->cursorPosition = 0;
-	this->choice = 0;
+	this->choice = -1;
+	this->menuPage = 0;
 }
 
 Menu::Menu(int windowSize)
@@ -13,7 +14,8 @@ Menu::Menu(int windowSize)
 	this->windowSize = windowSize;
 	this->name = "";
 	this->cursorPosition = 0;
-	this->choice = 0;
+	this->choice = -1;
+	this->menuPage = 0;
 }
 
 Menu::Menu(int windowSize, string name)
@@ -21,7 +23,8 @@ Menu::Menu(int windowSize, string name)
 	this->windowSize = windowSize;
 	this->name = name;
 	this->cursorPosition = 0;
-	this->choice = 0;
+	this->choice = -1;
+	this->menuPage = 0;
 }
 
 Menu::Menu(int windowSize, string name, vector<string> elements)
@@ -30,36 +33,127 @@ Menu::Menu(int windowSize, string name, vector<string> elements)
 	this->name = name;
 	this->elements = elements;
 	this->cursorPosition = 0;
-	this->choice = 0;
+	this->choice = -1;
+	this->menuPage = 0;
 }
 
 Menu::~Menu() 
 {
 }
 
-int Menu::addElem(string textToDisplay)
+void Menu::addElem(string textToDisplay)
 {
 	this->elements.push_back(textToDisplay);
-	return 0;
 }
 
-int Menu::removeElem(int elemToDel)
+void Menu::removeElem(int elemToDel)
 {
 	this->elements.erase(elements.begin()+elemToDel);
-	return 0;
 }
 
-int Menu::show() 
+void Menu::show() 
 {
+	system("cls");
 	cout << setw(this->windowSize) << this->name << endl;
-	for (int i = 0; i < elements.size(); i++) {
-		if (i == cursorPosition)
-			cout << "    ->  ";
-		else
-			cout << "\t";
+	if ((int)this->elements.size() <= elemsPerPage)
+	{
+		for (int i = 0; i < (int)this->elements.size(); i++)
+		{
+			if (i == this->cursorPosition)
+				cout << CURSOR;
+			else
+				cout << "\t";
 
-		cout << i << ") " << elements[i] << endl;
+			cout << i << ") " << this->elements[i] << endl;
+		}
 	}
+	else
+	{ 
+		if ((menuPage * elemsPerPage) < (int)this->elements.size())
+		{
+			for (int i = 0; i < elemsPerPage; i++) 
+			{
+				if ((i + menuPage*elemsPerPage) == this->cursorPosition)
+					cout << CURSOR;
+				else
+					cout << "\t";
 
-	return 0;
+				cout << i << ") " << this->elements[i+menuPage*elemsPerPage] << endl;  //  tu
+			}
+			cout << endl << "Uzyj strzalek w lewo oraz w prawo aby przejsc pomiedzy stronami menu." << endl;
+		}
+	}
+	cout << "Nacisnij ESC zeby wyjsc...";
+}
+
+int Menu::use() 
+{
+	int pressedKey;
+	this->choice = -1;
+	do
+	{
+		this->show();
+		pressedKey = _getch();
+		this->handlePressedKey(pressedKey);
+	} while ((!this->choiceMade()) && (pressedKey != KEY_ESC));
+
+	return this->choice;
+}
+
+void Menu::moveCursor()
+{
+	int pressedKey = _getch();
+	switch(pressedKey)
+	{ 
+		case KEY_ARROWUP:
+			this->moveCursorUp();
+			break;
+
+		case KEY_ARROWDOWN:
+			this->moveCursorDown();
+			break;
+
+		default:
+			break;
+	}
+}
+
+void Menu::moveCursorUp()
+{
+	if (this->cursorPosition == 0)
+		this->cursorPosition = (int)this->elements.size() - 1;
+	else
+		this->cursorPosition--;
+}
+
+void Menu::moveCursorDown()
+{
+	if (this->cursorPosition == (int)this->elements.size() - 1)
+		this->cursorPosition = 0;
+	else
+		this->cursorPosition++;
+}
+
+void Menu::handlePressedKey(int pressedKey)
+{
+	if (pressedKey == KEY_ARROWSPECIAL)
+		this->moveCursor();
+	else if (pressedKey == KEY_ENTER)
+		this->choice = this->cursorPosition;
+	else 
+	{
+		for (int i = 0; i < (int)this->elements.size(); i++)
+		{
+			if (pressedKey == KEY_0 + i)
+				this->choice = i;
+		}
+	}
+}
+
+bool Menu::choiceMade() 
+{
+	if (this->choice == -1) 
+		return false;
+	else 
+		return true;
 }
--- Katalog Multimediow/Katalog Multimediow/Menu.h
+++ Katalog Multimediow/Katalog Multimediow/Menu.h
@@ -2,7 +2,18 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+#include <cstdlib>
+#include <conio.h>
 using namespace std;
+
+#define	KEY_ESC	27
+#define KEY_ARROWSPECIAL 224
+#define KEY_ARROWUP 72
+#define KEY_ARROWDOWN 80
+#define KEY_ENTER 13
+#define KEY_0 48
+#define elemsPerPage 10
+#define CURSOR "    ->  "
 
 #pragma once
 class Menu
@@ -13,7 +24,14 @@ private:
 	vector<string> elements;
 	int cursorPosition;
 	int choice;
+	int menuPage;
 
+	void show();
+	void moveCursor();
+	void moveCursorUp();
+	void moveCursorDown();
+	void handlePressedKey(int pressedKey);
+	bool choiceMade();
 public:
 	Menu();
 	Menu(int windowSize);
@@ -21,9 +39,9 @@ public:
 	Menu(int windowSize, string name, vector<string> elements);
 	~Menu();
 
-	int addElem(string textToDisplay);
-	int removeElem(int elemToDel);
-	int show();
-
+	void addElem(string textToDisplay);
+	void removeElem(int elemToDel);
+	
+	int use();
 };
 
